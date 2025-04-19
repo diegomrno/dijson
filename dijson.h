601f9561dc7fc320065cde42019c3cea325ed507
diegomrno <diegomoreno.pro45@gmail.com>
2025-04-19 14:29:58 +0200

    init

---
+++ dijson.h
@@ -0,0 +1,17 @@
+/**
+ *   __
+ *  /\ \  __    __
+ *  \_\ \/\_\  /\_\    ____    ___     ___
+ *  /'_` \/\ \ \/\ \  /',__\  / __`\ /' _ `\
+ * /\ \L\ \ \ \ \ \ \/\__, `\/\ \L\ \/\ \/\ \
+ * \ \___,_\ \_\_\ \ \/\____/\ \____/\ \_\ \_\
+ *  \/__,_ /\/_/\ \_\ \/___/  \/___/  \/_/\/_/
+ *             \ \____/
+ *              \/___/
+ *
+ * dijson 1.0 -  Modern, safe, header-only & minimalist json utility
+ *               written in C
+ */
+
+// Copyright (c) 2025 - Diego Moreno
+// This software is licensed under the MIT license (see LICENSE.txt)
---
+++ example/main.c
@@ -0,0 +1,6 @@
+#include "../dijson.h"
+
+int main() {
+    
+  return 0;
+}
